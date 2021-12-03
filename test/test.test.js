bd014eca25a8628818f8fa58367ece1d943279a1
Samuel <samuel.schmidt@tuni.fi>
2021-12-03 16:13:40 +0200

    Base tests for all functions

--- test/test.test.js
+++ test/test.test.js
@@ -4,10 +4,12 @@ import isBuffer from "../src/isBuffer"
 import { Buffer } from 'buffer'
 import difference from "../src/difference"
 import chunk from "../src/chunk"
-
-function testfunc(a){
-    return a
-}
+import countBy from "../src/countBy"
+import filter from "../src/filter"
+import exp from "constants"
+import { isArrayBuffer, isTypedArray } from "util/types"
+import isArrayLike from "../src/isArrayLike"
+import isLength from "../src/isLength"
 
 describe("Tests for toNumber",()=>{
     test("Test number 1",()=>{
@@ -78,5 +80,100 @@ describe("Tests for chunk",()=>{
     })
     test("Test chunks with size 1",()=>{
         expect(chunk(arrchunk,1)).toEqual([['a'],['b'],['c'],['d']])
+    })
+})
+describe("Tests for countBy",()=>{
+    test("Test with valid data",()=>{
+        const users = [
+            { 'user': 'barney', 'active': true },
+            { 'user': 'betty', 'active': true },
+            { 'user': 'fred', 'active': false }
+        ]
+        expect(countBy(users, value => value.active)).toEqual({ 'true': 2, 'false': 1 })
+    })
+    test("Test with invalid data",()=>{
+        const users = [
+            { 'user': 'barney', 'active': true },
+            { 'user': 'betty', 'active': true },
+            { 'user': 'fred', 'active': false }
+        ]
+        expect(countBy(users, value => value.ok)).toEqual({})
+    })
+    test("Test with empty data",()=>{
+        const users = []
+        expect(countBy(users, value => value.bop)).toEqual({})
+    })
+    test("Test with empty object",()=>{
+        const users = {}
+        expect(countBy(users, value => value.cbt)).toEqual({})
+    })
+})
+describe("Tests for filter",()=>{
+    test("Test with valid data",()=>{     
+        const users = [
+           { 'user': 'barney', 'active': true },
+           { 'user': 'fred',   'active': false }
+        ]
+        expect(filter(users, ({ active }) => active) ).toEqual([{ 'user': 'barney', 'active': true }])
+    })
+    test("Test with invalid data",()=>{
+        const users = [
+            { 'user': 'barney', 'active': true },
+            { 'user': 'fred',   'active': false }
+        ]
+        expect(filter(users, ({active}) => active.bop)).toEqual([[]])
+    })
+    test("Test with empty data", ()=>{
+        const users = []
+        expect(filter(users, ({ active }) => active) ).toEqual([[]])
+    })
+    test("Test with null data", ()=>{
+        expect(filter(null, ({ active }) => active) ).toEqual([[]])
+    })
+})
+describe("Tests for isTypedArray",()=>{
+    test("Test with valid data",()=>{
+        const test = new Uint8Array
+        expect(isTypedArray(test)).toEqual(true)
+    })
+    test("Test with false valid data",()=>{
+        expect(isTypedArray([])).toEqual(false)
+    })
+    test("Test with invalid data",()=>{
+        expect(isTypedArray('A')).toEqual(false)
+    })
+    test("Test with empty data",()=>{
+        expect(isTypedArray('')).toEqual(false)
+    })
+    test("Test with null data", ()=>{
+        expect(isTypedArray(null)).toEqual(false)
+    })
+})
+describe("Tests for isArrayLike",()=>{
+    test("Test with true valid data",()=>{
+        expect(isArrayLike([1,2,3])).toEqual(true)
+    })
+    test("Test with false valid data",()=>{
+        expect(isArrayLike(bop=>bop)).toEqual(false)
+    })
+    test("Test with null data",()=>{
+        expect(isArrayLike(null)).toEqual(false)
+    })
+    test("Test with empty data",()=>{
+        expect(isArrayLike('')).toEqual(true)
+    })
+})
+describe("Tests for isLength",()=>{
+    test("Test with true valid data",()=>{
+        expect(isLength(3)).toEqual(true)
+    })
+    test("Test with false valid data",()=>{
+        expect(isLength('3')).toEqual(false)
+    })
+    test("Test with invalid data",()=>{
+        expect(isLength(9007199254740992)).toEqual(false)
+    })
+    test("Test with null data",()=>{
+        expect(isLength(null)).toEqual(false)
     })
 })
