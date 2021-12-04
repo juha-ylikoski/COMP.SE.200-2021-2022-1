4b83eefa444cfd6991d1db89d753868883696542
juha-ylikoski <juha.ylikoski14@outlook.com>
2021-12-04 15:52:33 +0200

    Added memoize test

--- test/memoize.test.js
+++ test/memoize.test.js
@@ -2,6 +2,35 @@
 import memoize from "../src/memoize"
 
 describe("Tests for memoize",()=>{
-    
+    test("Positive case", () => {
+        const object = { 'a': 1, 'b': 2 }
+        const f = memoize((arg)=>{
+            if (arg === 'a') {
+                return 1
+            } else if (arg == 'b') {
+                return 2
+            }
+        });
+        expect(f('a')).toEqual(1)
+        expect(f('b')).toEqual(2)
+    })
+    test("With resolver", () => {
+        const object = { 'a': 1, 'b': 2 }
+        const f = memoize((arg)=>{
+            if (arg === 'a') {
+                return 1
+            } else if (arg == 'b') {
+                return 2
+            }
+        }, (arg)=>{
+            arg
+        });
+        expect(f('a')).toEqual(1)
+        expect(f('b')).toEqual(2)
+    })
+    test("With bad args", () => {
+        expect(()=>memoize(1)).toThrow(TypeError)
+        expect(()=>memoize(1, 2)).toThrow(TypeError)
+    })
 })
 
